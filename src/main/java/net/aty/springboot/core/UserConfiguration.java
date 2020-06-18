1850298c946d907c66929a15b6092a5213bb5855
aitangyong <574007350@qq.com>
2020-06-19 00:40:34 +0800

    增加springboot的@EnableConfigurationProperties使用

--- src/main/java/net/aty/springboot/core/UserConfiguration.java
+++ src/main/java/net/aty/springboot/core/UserConfiguration.java
@@ -1,12 +1,15 @@
 package net.aty.springboot.core;
 
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:user.properties")
+@EnableConfigurationProperties(RedisProperties.class)
 public class UserConfiguration {
 
     @Bean
--- src/main/java/net/aty/springboot/core/UserDemo.java
+++ src/main/java/net/aty/springboot/core/UserDemo.java
@@ -2,15 +2,19 @@ package net.aty.springboot.core;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDemo implements InitializingBean {
     @Autowired
     private UserProperties userProperties;
+    @Autowired
+    private RedisProperties redisProperties;
 
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("userProperties..." + userProperties);
+        System.out.println("redisProperties..." + redisProperties);
     }
 }
